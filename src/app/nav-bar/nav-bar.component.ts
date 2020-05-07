3fd8cdb1707fea47406cbf08b511c0213331232c
Sebastian-Ene <ene.sebastian.2000@gmail.com>
2020-05-07 09:41:12 +0300

    Nav-bar changes, added links, added multiple pages

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AboutComponent } from '../about/about.component';
 import { ArtComponent } from '../art/art.component';
+import { NONE_TYPE } from '@angular/compiler';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,6 +10,15 @@ import { ArtComponent } from '../art/art.component';
 })
 export class NavBarComponent implements OnInit {
   constructor() {}
-
+  view = 'none';
+  showSocials() {
+    if (this.view === 'block') {
+      this.view = 'none';
+      return this.view;
+    } else {
+      this.view = 'block';
+      return this.view;
+    }
+  }
   ngOnInit(): void {}
 }
